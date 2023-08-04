cf0604fa459f7e978423dbb385c3acc5d59c5519
blazingbunny <140166379+blazingbunny@users.noreply.github.com>
2023-08-05 04:43:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -22,23 +22,17 @@ def calculate_similarity(texts):
     pairwise_similarity = cosine_similarity(vectorizer)
     return pairwise_similarity.mean()
 
-
-
 def score_keyword_distribution(url):
     """Return the relevancy scores of the h-tags in the HTML of the given URL."""
     html = get_html(url)
-    scores = defaultdict(list)
-    prev_tag_contents = {tag: [] for tag in ['h1', 'h2', 'h3', 'h4', 'h5']}
-    for tag in ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']:
-        contents = get_tag_contents(html, tag)
-        for content in contents:
-            if tag != 'h1':
-                prev_tag = f'h{int(tag[1]) - 1}'
-                scores[f'{prev_tag}-{tag}'].append(calculate_similarity(prev_tag_contents[prev_tag] + [content]))
-            prev_tag_contents[tag].append(content)
-    avg_scores = {tag_pair: sum(scores) / len(scores) for tag_pair, scores in scores.items() if scores}
-    return avg_scores
-
+    soup = BeautifulSoup(html, 'html.parser')
+    headings = [('h1', tag.get_text()) for tag in soup.find_all(['h1', 'h2', 'h3', 'h4', 'h5', 'h6'])]
+    scores = []
+    for i in range(1, len(headings)):
+        if int(headings[i][0][1]) > int(headings[i-1][0][1]):
+            score = calculate_similarity([headings[i-1][1], headings[i][1]])
+            scores.append((headings[i-1][0], headings[i-1][1], headings[i][0], headings[i][1], score))
+    return scores
     
 def calculate_similarity(texts):
     """Return the average cosine similarity of the given texts."""
